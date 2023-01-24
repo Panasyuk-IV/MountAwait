02110815305c60575c3729ab9fcedb3fbef837c9
Panasyuk -IV <vaaaans8@gmail.com>
2023-01-24 18:44:21 +0300

    first version site

--- mountaintrip/mountaintrip/wsgi.py
+++ mountaintrip/mountaintrip/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Mounttrip project.
+WSGI config for mountaintrip project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mounttrip.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mountaintrip.settings')
 
 application = get_wsgi_application()
